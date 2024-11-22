05889349542c6af2ba0bbf6360bd3725b0006811
Nguyễn Văn Ước <uocnv.soict.hust@gmail.com>
2024-11-22 10:58:12 +0700

    fix

--- resources/views/layouts/modal_slide_right.blade.php
+++ resources/views/layouts/modal_slide_right.blade.php
@@ -10,11 +10,11 @@
          data-modal-open="{{$open}}"
             {{$staticBackdrop ? "data-bs-backdrop=static" : ''}}
     >
-        <div class="modal-dialog modal-fullscreen-md-down modal-dialog-scrollable h-100 m-0 ms-auto {{$size}} {{$type}}"
+        <div class="modal-dialog modal-fullscreen-md-down {{$size}} {{$type}}"
              role="document"
              id="screen-modal-type-{{$key}}"
         >
-            <form class="modal-content h-100"
+            <form class="modal-content"
                   action="{{ $method }}"
                   id="screen-modal-form-{{$key}}"
                   method="post"
@@ -22,6 +22,38 @@
                   data-controller="form"
                   data-action="form#submit"
             >
+                <div class="p-4 modal-placeholder">
+                    <div class="placeholder-glow mb-3 mt-1 d-flex align-items-center">
+                        <span class="placeholder col-6 h5 me-auto rounded-1"></span>
+                        <button type="button" class="btn-close" title="Close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <p class="placeholder-glow">
+                        <span class="placeholder col-7 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-6 rounded-1"></span>
+                        <span class="placeholder col-8 rounded-1"></span>
+                        <span class="placeholder col-7 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-6 rounded-1"></span>
+                        <span class="placeholder col-8 rounded-1"></span>
+                        <span class="placeholder col-3 rounded-1"></span>
+                        <span class="placeholder col-7 rounded-1"></span>
+                    </p>
+
+                    <p class="placeholder-glow mb-0">
+                        <span class="placeholder col-3 rounded-1"></span>
+                        <span class="placeholder col-7 rounded-1"></span>
+                        <span class="placeholder col-7 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                        <span class="placeholder col-6 rounded-1"></span>
+                        <span class="placeholder col-8 rounded-1"></span>
+                        <span class="placeholder col-4 rounded-1"></span>
+                    </p>
+                </div>
                 <div class="modal-header">
                     <h4 class="modal-title text-black fw-light" data-modal-target="title">{{$title}}</h4>
                     <button type="button" class="btn-close" title="Close" data-bs-dismiss="modal"
